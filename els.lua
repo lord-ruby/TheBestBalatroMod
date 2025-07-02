c22fa84668b18080d54a21ab022a67407ca50639
Ruby <darkdragongaming379@gmail.com>
2025-07-02 12:13:38 +0100

    add stuff

---
+++ els.lua
@@ -0,0 +1,39 @@
+ SMODS.Joker{
+    key = "ultimate_joker",
+    rarity = 1,
+    price = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    demicoloncompat = true,
+    config = {
+        extra = 1e308
+    },
+    atlas = "esl_joker",
+    pos = {x=0,y=0},
+    soul_pos = {x=1,y=0},
+    loc_vars = function(self, q, card)
+        return {
+            vars = {
+                "{1.79769e308}",
+                card.ability.extra
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                hypermult = {
+                    1.79769e308,
+                    card.ability.extra
+                }
+            }
+        end
+    end
+}
+
+SMODS.Atlas{
+    key = "esl_joker",
+    path="esl_joker.png",
+    px = 71,
+    py = 95
+}
---
+++ localization/en-us.lua
@@ -0,0 +1,13 @@
+return {
+    descriptions = {
+        Joker = {
+            j_els_ultimate_joker = {
+                name = "The Best Joker in All of Modded Balatro",
+                text = {
+                    "You {C:red}literally{} cannot beat it",
+                    "{X:dark_edition,C:white}#1##2#{} Mult"
+                }
+            }
+        }
+    }
+}
